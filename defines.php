754bee50aed53ae69931ca31a00fb8a764b1aed6
coderius <sunrise4fun@gmail.com>
2019-03-11 13:55:28 +0200

    Add Url::to for top nav links

--- defines.php
+++ defines.php
@@ -1,12 +1,12 @@
 <?php
 
-/**
+/*
  * @package myblog
  * @file config.php created 13.08.2018 18:09:23
- * 
+ *
  * @copyright Copyright (C) 2018 Sergio coderius <coderius>
  * @license This program is free software: GNU General Public License
  */
 
-defined('YII_WORK_SERVER') or define('YII_WORK_SERVER', true);
-
+ //In local server this option must be set to false.
+defined('YII_WORK_SERVER') or define('YII_WORK_SERVER', false);
